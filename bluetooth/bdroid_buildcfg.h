97a5c3400f284f213b21116000fd57ed71624106
XperiAMM <andresmora1676@gmail.com>
2015-10-09 20:19:54 -0600

    Bluetooth and camera x2 fix

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,5 +1,7 @@
 /*
- * Copyright (C) 2015 MaxiCM Team
+ * Copyright (C) 2012 The Android Open Source Project
+ * Copyright (C) 2014 The CyanogenMod Project
+ * Copyright (C) 2015 The MaxiCM Team Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,10 +20,5 @@
 #define _BDROID_BUILDCFG_H
 
 #define BTM_DEF_LOCAL_NAME   "LG G3 Beat"
-#define BLUETOOTH_QTI_SW TRUE
-// Disables read remote device feature
-#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
-#define MAX_L2CAP_CHANNELS    16
-// skips conn update at conn completion
-#define BTA_BLE_SKIP_CONN_UPD  FALSE
+#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 #endif
